c8be380c9845f214108986eaadf98687969def07
inkku3 <ingrid.kreula@gmail.com>
2025-05-31 15:40:19 +0300

    Fixed typos in questions.

--- src/specialEvents.js
+++ src/specialEvents.js
@@ -5,9 +5,11 @@ export const specialEvents = [
   { text: "Kun yksi juo, kaikki juo - 2 kierroksen ajan.", handler: "rounds", roundsName: "KAIKKI JUO", roundLength: "2" },
   { text: "'KEISARI' = juo.", handler: "youtube", videoId: "UfrdOr-OAPA"},
   { text: "'PLEASE' = juo.", handler: "youtube", videoId: "YiQ7qiL73aI"},
+  { text: "'THUNDER' = juo.", handler: "youtube", videoId: "fKopy74weus"},
   { text: "Ei puhelimia kierrokset! Jokainen laittaa puhelimensa näkyville. Puhelimensa hapuilija juo.", handler: "rounds", roundsName: "EI PUHELIMIA", roundLength: "3"},
   { text: "Valitse kielletty sana, jota ei saa sanoa seuraavan kierroksen aikana! Rangaistus on juominen.", handler: "rounds", roundsName: "KIELLETTY SANA", roundLength: "1"},
   { text: "Kaikki laittavat mitä tahansa hatuksi. Jos jonkun hattu tippuu kierroksen aikana, hän juo.", handler: "rounds", roundsName: "HATTUAIKA", roundLength: "1"},
   { text: "10min mallasmaraton: 50ml juomaa per PIIP. Alko jo!", handler: "timer", minutes: "10"},
   { text: "Ei naurua 2 kierrokseen. Jos nauraa, joutuu ottamaan 3 hörppyä", handler: "rounds", roundsName: "EI NAURUA", roundLength: "2"}
+
 ];
